4415bb603a884a4b2e475826a3d50b318385bff5
islishude <islishude@gmail.com>
2022-09-17 20:50:43 +0800

    feat: object and array param(close #1)

---
+++ param_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parseArgs(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    []string
+		want    []interface{}
+		wantErr bool
+	}{
+		{"eth_getBlockByNumber", []string{"100", "true"}, []interface{}{"0x64", true}, false},
+		{"eth_getBlockByNumber_2", []string{"latest", "false"}, []interface{}{"latest", false}, false},
+		{"eth_eastimateGas", []string{`{"from": "1","to": "2"}`, "latest"}, []interface{}{map[string]interface{}{"from": "1", "to": "2"}, "latest"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseArgs(tt.args)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseArgs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseArgs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
